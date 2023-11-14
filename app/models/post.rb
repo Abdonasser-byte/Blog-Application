cd5ed1cb8125f4cc5699e91127d154088b54dd7e
AbdoN <abdon2078@gmail.com>
2023-11-14 19:56:22 +0200

    blogapp

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
 
+  after_create :schedule_cleanup
+
+  
   validates :title, presence: true
   validates :body, presence: true
   validates :tags, presence: true
@@ -12,6 +15,13 @@ class Post < ApplicationRecord
   def at_least_one_tag
     errors.add(:base, 'A post must have at least one tag') if tags.empty?
   end
+
+  private
+
+  def schedule_cleanup
+    PostCleanupWorker.perform_in(24.hours, id)
+  end
+
 end
 
 
--- app/sidekiq/post_deletion_job.rb
+++ app/sidekiq/post_deletion_job.rb
@@ -3,6 +3,12 @@ class PostDeletionJob
 
   def perform(post_id)
     post = Post.find_by(id: post_id)
-    post&.delete_with_associated
+    if post && post.created_at < 24.hours.ago
+      post.destroy
+      post&.delete_with_associated
+      Rails.logger.info "Post #{post_id} deleted."
+    else
+      Rails.logger.info "Post #{post_id} not found or not eligible for deletion."
+    end
   end
 end
